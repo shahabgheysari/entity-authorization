863947563fe18e2aeeb1d1ef0910bf3017cca601
shahab gheysari <shahabgheysari@gmail.com>
2019-07-03 00:06:18 +0430

    Added service provider

---
+++ src/PAServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Shahab\PA;
+
+use Illuminate\Support\ServiceProvider;
+
+class PAServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes(
+            [
+            __DIR__.'/migrations' => database_path('migrations')
+            ]
+        , 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
