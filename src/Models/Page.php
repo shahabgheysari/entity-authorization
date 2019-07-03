a873d3940d8140494448adfcb98704062c941e72
shahab gheysari <shahabgheysari@gmail.com>
2019-07-04 00:22:59 +0430

    overrode RBAC trait function

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -5,19 +5,30 @@ namespace Shahab\PA\Models;
 use Illuminate\Database\Eloquent\Model;
 use DCN\RBAC\Traits\HasRoleAndPermission as HasRoleAndPermissionTrait;
 use DCN\RBAC\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
+use Shahab\PA\Traits\Authorize;
 
 class Page extends Model implements HasRoleAndPermissionContract
 {
-    use HasRoleAndPermissionTrait;
+    use HasRoleAndPermissionTrait,Authorize;
     protected $fillable=['name'];
     protected $tablename="pages";
-    public $pivotTableName="permission_page";
+    //public $pivotTableName="permission_page";
 
     public function roles(){
-        return $this->belongsToMany(config('rbac.models.role'),'role_page')->withTimestamps()->withPivot('granted');
+        return $this->belongsToMany(config('rbac.models.role'),'role_page')->withPivot('granted');
     }
 
     public function permissions(){
+        return $this->belongsToMany(config('rbac.models.permission'),'permission_page')->withPivot('granted');
+    }
+
+     /**
+     * override RBAC trait function
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function userPermissions()
+    {
         return $this->belongsToMany(config('rbac.models.permission'),'permission_page')->withTimestamps()->withPivot('granted');
     }
     
